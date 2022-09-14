74e0a0d909e5b00da45b975127f116c5b8c781e8
revathivasu9999 <revathivasu.r@gmail.com>
2022-09-14 11:26:55 +0530

    sixth Commit

--- reactivevalidation/reactivevalidation.component.ts
+++ reactivevalidation/reactivevalidation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivevalidation',
@@ -8,61 +8,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactivevalidationComponent implements OnInit {
   reactiveForm!: FormGroup;
-  submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  submitted:boolean = false;
+  constructor(private formBuilder: FormBuilder) { 
     this.reactiveForm = this.formBuilder.group({
-      
       FullName: ['',Validators.required],
       Address: ['',Validators.required],
       City: ['',Validators.required],
       Email: ['',[Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       Phoneno:['',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
-      password: ['',[Validators.required, Validators.minLength(6)]],
-      Confirmpassword: ['', Validators.required],
-      terms:true
-    },
-      { 
-        validator: this.MustMatch('password', 'Confirmpassword'),
-     
-      }
-       
-  );
-  
+      password:new FormControl(null,[Validators.required]),
+     confirmpassword:new FormControl(null,[Validators.required])
+},
+{
+validators:this.MustMatch('password','confirmpassword')
+})
 }
-
 get f(){
-  return this.reactiveForm.controls;
- }
- MustMatch(password:any, Confirmpassword:any){
-  return(formGroup:FormGroup)=>{
-    const passwordcontrol=formGroup.controls[password];
-    const ConfirmpasswordControl=formGroup.controls[Confirmpassword];
-
-    if(password.errors && !Confirmpassword.errors.MustMatch){
-      return;
-    }
-    if(passwordcontrol.value !== ConfirmpasswordControl.value){
-      passwordcontrol.setErrors({MustMatch:true});
-    }
-    else{
-      ConfirmpasswordControl.setErrors(null);
-    }
-  }
- }
- 
-
-
- onSubmit() {
-  
-      //console.log(this.reactiveForm.value); 
-      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.reactiveForm.value))
-
-  }
-  
- 
-onclick() {
-  
+return this.reactiveForm.controls
 }
+MustMatch(password:string,matchingpassword:string){
+return(formGroup:FormGroup)=>{
+const control=formGroup.controls[password];
+const matchingcontrol=formGroup.controls[matchingpassword];
+if (matchingcontrol.errors && !matchingcontrol.errors.MustMatch){
+  return
+}
+if (control.value!==matchingcontrol.value){
+  matchingcontrol.setErrors({MustMatch:true})
+}
+else{
+  matchingcontrol.setErrors(null)
+}
+}
+}
+onSubmit(){
+this.submitted=true;
+if(this.reactiveForm.invalid){
+return
+}
+else{
+  alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.reactiveForm.value))
+}
+}
+
+ngOnInit(): void {
+}
+
 }
